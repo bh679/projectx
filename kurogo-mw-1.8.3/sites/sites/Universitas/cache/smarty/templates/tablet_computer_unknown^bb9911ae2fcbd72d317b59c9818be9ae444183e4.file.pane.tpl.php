36fc5496bd46ef62ea1f556889c5cc1e4b41943f
David Mattner <dm940@uowmail.edu.au>
2013-09-28 03:02:42 -0700

    Signed-off-by: David Mattner <dm940@uowmail.edu.au>

---
+++ kurogo-mw-1.8.3/sites/sites/Universitas/cache/smarty/templates/tablet_computer_unknown^bb9911ae2fcbd72d317b59c9818be9ae444183e4.file.pane.tpl.php
@@ -0,0 +1,21 @@
+<?php /* Smarty version Smarty-3.0.7, created on 2013-09-28 03:15:05
+         compiled from "/Users/Rory1/Documents/projectx/kurogo-mw-1.8.3/Kurogo-Mobile-Web/app/modules/home/templates/pane.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:602389995524681f9daa985-42569168%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'bb9911ae2fcbd72d317b59c9818be9ae444183e4' => 
+    array (
+      0 => '/Users/Rory1/Documents/projectx/kurogo-mw-1.8.3/Kurogo-Mobile-Web/app/modules/home/templates/pane.tpl',
+      1 => 1364681343,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '602389995524681f9daa985-42569168',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+)); /*/%%SmartyHeaderCode%%*/?>
+<?php echo $_smarty_tpl->getVariable('content')->value;?>
+
